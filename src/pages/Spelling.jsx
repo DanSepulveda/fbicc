a363327e1a9cd21067b63790a70a12788ca1a4ff
Daniel Sepúlveda <dansepdev@gmail.com>
2024-10-05 01:16:04 -0300

    responsive ok

--- src/pages/Spelling.jsx
+++ src/pages/Spelling.jsx
@@ -1,12 +1,11 @@
+import { FaArrowRightLong } from 'react-icons/fa6'
 import { useFetch } from '../hooks/useFetch'
 import { useWord } from '../hooks/useWord'
 import Button from '../components/Button'
 import Input from '../components/Input'
-import Title from '../components/Title'
 
-// TODO: check styles
 const Spelling = () => {
-  const [data] = useFetch([], '/data/words.json')
+  const [data] = useFetch('/data/words.json')
   const { selected, checkAnswer } = useWord(data)
 
   const handleSubmit = (event) => {
@@ -18,11 +17,14 @@ const Spelling = () => {
   }
 
   return (
-    <div>
-      <Title>Spell words</Title>
-      <p className="bg-red-800 text-4xl p-2 py-3 text-center text-white mb-5 rounded-md">
-        {selected.word}
-      </p>
+    <div className="max-w-3xl mx-auto">
+      <div className="border rounded-md overflow-hidden text-center shadow-lg mb-6">
+        <div className="bg-red-800 text-white text-2xl font-bold py-1 font-ubuntu tracking-widest">
+          WORD
+        </div>
+        <p className="text-4xl text-gray-800 font-medium py-3">{selected.word}</p>
+      </div>
+
       <form
         className="flex flex-col gap-4"
         onSubmit={handleSubmit}
@@ -30,11 +32,16 @@ const Spelling = () => {
         <Input
           label="Your answer"
           name="spelling"
-          placeholder="Use a space to separate letters"
-          message="Example: kabar -> ka a be a er"
+          placeholder="Use a space as separator"
+          message={
+            <span className="flex items-center gap-2 text-cyan-700">
+              Example: kabar <FaArrowRightLong />
+              ka a be a er
+            </span>
+          }
         />
         <div className="flex justify-center">
-          <Button type="submit">Check</Button>
+          <Button type="submit">CHECK</Button>
         </div>
       </form>
     </div>
