16284b625463fd90dc1d4c5713d9c05afdb48c46
Daniel Sepúlveda <dansepdev@gmail.com>
2024-10-05 02:22:29 -0300

    functions ok

--- src/lib/getRandom.js
+++ src/lib/getRandom.js
@@ -1,14 +1,8 @@
-// !ok
 export const randomFromArray = (list) => {
   const random = Math.floor(Math.random() * list.length)
   return list[random]
 }
 
-// !ok
 export const getRandomNumber = (number) => {
   return Math.floor(Math.random() * (number + 1))
-}
-
-export const getRandomTime = () => {
-  return null
 }
