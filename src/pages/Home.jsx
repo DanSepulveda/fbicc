8c9f6478a647ba9668a8ddd961a1200594a7963d
Daniel Sepúlveda <dansepdev@gmail.com>
2024-10-05 00:33:48 -0300

    responsive ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,27 +1,29 @@
 import { Link } from 'react-router-dom'
 import { useFetch } from '../hooks/useFetch'
-import Title from '../components/Title'
-
-// TODO: check article styles
+import Loader from '../components/Loader'
 
 const Home = () => {
-  const [data] = useFetch([], '/data/activities.json')
+  const [data, loading] = useFetch('/data/activities.json')
+
+  if (loading) {
+    return <Loader />
+  }
 
   return (
     <div>
-      <Title>Activities</Title>
-      <div className="grid grid-cols-3 gap-3">
+      <div className="grid grid-cols-3 lg:grid-cols-4 gap-3 lg:gap-5">
         {data.map((block) => (
           <Link
             key={block.name}
             to={block.link}
           >
-            <article className="bg-white rounded-md pt-2.5 shadow-lg">
+            <article className="bg-white hover:bg-red-100 transition rounded-md pt-2.5 shadow-lg">
               <img
                 src={'/images/' + block.image}
                 className="h-12 mx-auto"
+                alt={block.name}
               />
-              <h2 className="text-white text-lg bg-red-900 font-semibold font-ubuntu text-center mt-3  py-0.5 rounded-b-md">
+              <h2 className="text-white text-lg bg-gray-500 font-semibold font-ubuntu text-center mt-3  py-0.5 rounded-b-md">
                 {block.name}
               </h2>
             </article>
